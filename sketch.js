b0d21a01e79f70fb672a05e30fc932a4478ead0f
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-11 16:05:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,16 +18,16 @@ function draw() {
      noFill();
 
      let secondAngle = map(sc, 0, 60, 0, 360);
-     // arc(0, 0, 300, 300, 0, secondAngle);
+      arc(0, 0, 300, 300, 0, secondAngle);
 
 
      stroke(150, 100, 255);
      let minuteAngle = map(mn, 0, 60, 0, 360);
-     // arc(0, 0, 280, 280, 0, minuteAngle);
+      arc(0, 0, 280, 280, 0, minuteAngle);
 
      stroke(150, 255, 100);
      let hourAngle = map(hr % 12, 0, 12, 0, 360);
-     // arc(0, 0, 260, 260, 0, hourAngle);
+     arc(0, 0, 260, 260, 0, hourAngle);
 
 
 
@@ -52,25 +52,6 @@ function draw() {
 
      stroke(255);
      point(0, 0);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 }
