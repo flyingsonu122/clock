0a0660a6de64318319c9ec0d18e5a01a69c27143
Sonu Kumar Kushwaha <flyingsonu736@gmail.com>
2021-09-18 13:25:57 +0530

    Add files via upload

--- clock.js
+++ clock.js
@@ -17,8 +17,17 @@ function draw() {
     strokeWeight(2);
     textSize(32);
     fill(255);
-
-    text(hr % 12 + ' : ' + mn + " : " + sc, 130, 35);
+    textAlign(CENTER, CENTER);
+ 
+    
+    if(hr>12){
+      
+     text(hr%12 + ' : ' + mn + ' : ' + sc, width/2, 35);
+   
+    } else {
+     text(hr + ' : ' + mn + ' : ' + sc, width/2, 35 );
+      
+     }
 
 
     
@@ -28,11 +37,28 @@ function draw() {
     fill(0);
     arc(0, 0, 250, 250, 0, 360);
 
+    push();
+    rotate(90);
+    fill(255);
+    textAlign(CENTER,CENTER);
     
+    
+    translate(-100,-100);
+   
+    text("12",100,0);
+    text("9",0,100);
+    text("3",200,100);
+    text("6",100,200);
+    
+    pop();
+  
     
     strokeWeight(8);
     stroke(255, 100, 150);
     noFill();
+    
+    
+   
 
     let secondAngle = map(sc, 0, 60, 0, 360);
     //  arc(0, 0, 300, 300, 0, secondAngle);
@@ -51,19 +77,19 @@ function draw() {
     push();
     rotate(secondAngle);
     stroke(255, 100, 150);
-    line(0, 0, 100, 0);
+    line(0, 0, 80, 0);
     pop();
 
     push();
     rotate(minuteAngle);
     stroke(150, 100, 255);
-    line(0, 0, 75, 0);
+    line(0, 0, 60, 0);
     pop();
 
     push();
     rotate(hourAngle);
     stroke(150, 255, 100);
-    line(0, 0, 50, 0);
+    line(0, 0, 40, 0);
     pop();
 
 
@@ -72,3 +98,8 @@ function draw() {
 
 
 }
+
+
+
+
+
